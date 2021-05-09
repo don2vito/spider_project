91dc6946a7fe5b001486f7b34b95c284f7c79028
don2vito <43207483+don2vito@users.noreply.github.com>
2021-05-09 11:30:33 +0800

    更新代码

--- 晨报自动化/销售表/sales_table_process_plus.py
+++ 晨报自动化/销售表/sales_table_process_plus.py
@@ -124,7 +124,7 @@ def table_process(df):
     df['品牌编号'] = df['品牌编号'].apply(func_brand_id)
     
     cols = [33,32,34,0,1,36,2,3,4,7,8,9,5,6,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,35,37,38]
-    df=df.iloc[:,cols]
+    df = df.iloc[:,cols]
     
     print(df.info())
     df.to_excel('./销售处理表.xlsx', sheet_name='table', index=False, encoding='utf-8')

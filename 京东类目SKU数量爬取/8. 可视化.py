35d9495aa1f6fc79dcb7981b7834d9b8c86f3297
don2vito <43207483+don2vito@users.noreply.github.com>
2022-10-15 16:42:07 +0800

    更新代码

--- 京东类目SKU数量爬取/8. 可视化.py
+++ 京东类目SKU数量爬取/8. 可视化.py
@@ -1,7 +1,10 @@
 import plotly.io as pio
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
 import pandas as pd
+import numpy as np
+
 
 df1 = pd.read_excel('./6. 输出京东类目SKU转换后数据.xlsx',sheet_name='result')
 
@@ -30,3 +33,61 @@ fig.update_layout(title='事业部SKU占比对比(%)')
 fig.update_traces(textposition='outside',textfont_size=16,textfont_color=['#FC5531'])
 pio.write_html(fig,'事业部SKU占比对比.html')
 pio.write_image(fig,'事业部SKU占比对比.png','png',width=1400,height=800)
+
+
+# JD 树状图
+df1['整体'] = '整体'
+fig1 = px.treemap(df1, 
+                 path=['整体', '大分类名', '中分类名'], 
+                 values='转换后SKU数', 
+                 title='京东类目SKU占比树状图',
+                 # color='转换后SKU数',
+                 # color_continuous_scale='RdBu',
+                 # color_continuous_midpoint=df1['转换后SKU数'].mean()
+                )
+fig1.update_traces(textinfo='label+value',textfont = dict(size = 20))                                                                                 
+pio.write_html(fig1,'JD类目SKU占比树状图.html')
+pio.write_image(fig1,'JD类目SKU占比树状图.png','png',width=1400,height=800)
+
+
+#  JD 热力图
+bins = [0,1,20000,50000,100000,150000,200000,300000,400000,500000,99999999999]
+groups1 = ['0','2万','5万','10万','15万','20万','30万','40万','50万','50万以上']
+groups2 = [.1,.2,.3,.4,.5,.6,.7,.8,.9,1.0]
+df1['SKU数级别'] = pd.cut(df1['转换后SKU数'],bins,labels=groups1)
+df1['SKU数级别'] = df1['SKU数级别'].fillna('0')
+
+data = df1.groupby(['大分类名','SKU数级别'])['转换后SKU数'].sum().reset_index()
+data = pd.pivot(data,values='转换后SKU数',index='大分类名',columns='SKU数级别')
+
+data2 = data.apply(lambda x:pd.cut(x,bins,labels=groups2))
+data2 = data2.fillna(.1)
+
+data = data.applymap(lambda x:str(round(x / 10000,2)) + ' 万')
+
+data.drop(index='众筹',columns='0',inplace=True)
+data2.drop(index='众筹',columns='0',inplace=True)
+
+x = list(data.columns)
+y = list(data.index)
+z = data2.values.tolist()
+z_text = data.fillna('').values.tolist()
+
+#  自定义色卡
+# colorscale = [[0.0,'rgb(0,153,102)'],
+#               [.1,'rgb(211,207,99)'],
+#               [.3,'rgb(255,153,51)'],
+#               [.4,'rgb(204,97,51)'],
+#               [.5,'rgb(102,0,153)'],
+#               [1.0,'rgb(126,0,35)']]
+
+fig2 = ff.create_annotated_heatmap(z,
+                                   x=x,
+                                   y=y,
+                                   annotation_text=z_text,
+                                   # colorscale=colorscale
+                                   )
+fig2.update_layout(title='京东类目SKU占比热力图')
+fig2.update_xaxes(side='top')
+pio.write_html(fig2,'JD类目SKU占比热力图.html')
+pio.write_image(fig2,'JD类目SKU占比热力图.png','png',width=1400,height=800)
